67f44a4cdbf4a8c57c03f95e7f3d581fa4194851
Chukwuma <echukwuma561@gmail.com>
2025-10-18 21:14:51 +0100

    wip

--- components/projects.tsx
+++ components/projects.tsx
@@ -37,7 +37,7 @@ const projects = [
     title: "StreamFlix",
     description:
       "A movie-streaming platform where studios and creators upload movies, and viewers can watch and enjoy them online.",
-    image: "/images/Streamflix.png",
+    image: "/images/kudora.png",
     tech: ["React.js", "TypeScript", "Tailwind CSS"],
     category: "Streaming Platform",
     status: "In Progress",
